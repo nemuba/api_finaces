1f5b01565ae82404e8fbd7057da403cfa8315af1
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2022-03-20 13:32:46 -0300

    add validations in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true
+  validates :password, length: { minimum: 6, maximum: 12 }
+  validates :name, presence: true
+
   api_guard_associations refresh_token: 'refresh_tokens'
   has_many :refresh_tokens, dependent: :delete_all
 end
